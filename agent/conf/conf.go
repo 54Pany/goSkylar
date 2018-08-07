f3b695073c2529915e294021c7536e7d49cb9436
wangshuo30 <wangshuo11@jd.com>
2018-08-07 16:16:04 +0800

    first commit

--- agent/conf/conf.go
+++ agent/conf/conf.go
@@ -3,11 +3,9 @@ package conf
 // agnet配置文件
 
 const (
-	//REDIS_URI      = "redis://root:a1x06awvaBpD@192.168.0.41:23177/5"
-	REDIS_URI      = "redis://root:a1x06awvaBpD@116.196.96.123:23177/5"
-
-	VERSION_URL    = "http://192.168.0.41/version"
-	DOWNLOAD_URL   = "http://116.196.96.123/download/linux/agent"
+	REDIS_URI      = "redis://root:password@100.0.0.1:6379/0"
+	VERSION_URL    = "http://192.168.0.1/version"
+	DOWNLOAD_URL   = "http://100.0.0.41/download/linux/agent"
 	BACK_FILE_PATH = "/tmp/"
-	CURR_VERSION   = "1.0.56"
+	CURR_VERSION   = "1.0.01"
 )
--- server/conf/conf.go
+++ server/conf/conf.go
@@ -3,9 +3,9 @@ package conf
 // agnet配置文件
 
 const (
-	REDIS_URI      = "redis://root:a1x06awvaBpD@116.196.96.123:23177/5"
-	MONGO_URI      = "mongodb://jdsea:jdsea000@172.20.222.88:80/sea?authMechanism=SCRAM-SHA-1"
-	MESSAGE_URI    = "http://172.17.55.143/services/MoblMsgSender"
-	MESSAGE_NUMBER = "13552085481,13717608441,18611511892"
+	REDIS_URI      = "redis://root:password@100.0.0.1:6379/0"
+	MONGO_URI      = "mongodb://admin:admin000@192.168.0.1/sea?authMechanism=SCRAM-SHA-1"
+	MESSAGE_URI    = "http://172.0.0.1/services/MoblMsgSender"
+	MESSAGE_NUMBER = "135xxxxxxxx"
 	PUBLIC_IP_PRIVATEUSE = "11,19,20,21,22"
 )
